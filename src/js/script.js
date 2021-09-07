c4dc069c271d007e6801713d205248a3ca7f08f2
Dekorate <d3k0rate@gmail.com>
2021-09-07 04:03:51 +0300

    70% adaptive + 50% burger menu

--- src/js/script.js
+++ src/js/script.js
@@ -16,30 +16,27 @@ function testWebP(callback) {
     }
     });
 
-/* Индекс слайда по умолчанию */
-var slideIndex = 1;
+// Main Slider
+
+let slideIndex = 1;
 showSlides(slideIndex);
 
-/* Функция увеличивает индекс на 1, показывает следующй слайд*/
 function plusSlide() {
     showSlides(slideIndex += 1);
 }
 
-/* Функция уменьшяет индекс на 1, показывает предыдущий слайд*/
 function minusSlide() {
     showSlides(slideIndex -= 1);  
 }
 
-/* Устанавливает текущий слайд */
 function currentSlide(n) {
     showSlides(slideIndex = n);
 }
 
-/* Основная функция слайдера */
 function showSlides(n) {
-    var i;
-    var slides = document.getElementsByClassName("slider-item");
-    var dots = document.getElementsByClassName("dots__item");
+    let i;
+    let slides = document.getElementsByClassName("slider-item");
+    let dots = document.getElementsByClassName("dots__item");
     if (n > slides.length) {
       slideIndex = 1
     }
