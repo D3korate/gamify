a0abb3c053adc2ec7b98939eb76c918a49253c7a
Your Name <lektordan@gmail.com>
2021-09-08 18:23:10 +0300

    test backup

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,84 @@
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    slidesPerView: 3,
+    spaceBetween: 30,
+    breakpoints:{
+        1140:{
+            slidesPerView: 3, 
+        },
+        771:{
+            slidesPerView: 2,
+            spaceBetween: 250,
+        },
+        720:{
+            slidesPerView: 2,
+            spaceBetween: 30,
+        },
+        650:{
+            slidesPerView: 2,
+            spaceBetween: 50,
+        },
+        649:{
+            slidesPerView: 1,
+            direction: 'vertical',
+        },
+        450:{
+            direction: 'vertical',
+            slidesPerView: 1,
+        }
+
+        
+    },
+    
+
+  
+    
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.review__wrapper-next',
+      prevEl: '.review__wrapper-prew',
+    },
+  
+    
+  });
+
+
+
+
+
+  let tab1 = document.querySelector("#tab1");
+  let tab2 = document.querySelector("#tab2");
+  let tabs = document.querySelectorAll(".tab");
+  let content = document.querySelectorAll(".content");
+  
+  tab1.onclick = () => {
+    if (tab1.checked) {
+      content[0].style.display = "block";
+      content[1].style.display = "none";
+      tabs[0].classList.add("plus");
+      tabs[1].classList.remove("plus");
+    }
+  };
+  
+  tab2.onclick = () => {
+    if (tab2.checked) {
+      content[0].style.display = "none";
+      content[1].style.display = "block";
+     
+      tabs[1].classList.add("plus");
+      tabs[0].classList.remove("plus");
+     
+    }
+  };
+
 const burger = document.querySelector('.header__burger');
 const burgerContent = document.querySelector('.header__burger-ul');
+
+
+
 
 burger.addEventListener('click', function() {
     burger.classList.toggle('active');
@@ -23,6 +102,10 @@ function testWebP(callback) {
     document.querySelector('body').classList.add('no-webp');
     }
     });
+
+
+
+ 
 
 // Main Slider
 
@@ -60,3 +143,4 @@ function showSlides(n) {
     slides[slideIndex - 1].style.display = "flex";
     dots[slideIndex - 1].className += " active";
 }
+
